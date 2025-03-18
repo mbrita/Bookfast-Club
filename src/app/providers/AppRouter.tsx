b71583026e9d7dc92c7fe9e9fee7ee696160051d
margarita <146252026+mbrita@users.noreply.github.com>
2025-03-18 22:52:01 +0300

    add header

--- src/app/providers/AppRouter.tsx
+++ src/app/providers/AppRouter.tsx
@@ -1,9 +1,12 @@
 import { Main } from '@/pages/Main';
+import Header from '@/widgets/Header';
+
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
