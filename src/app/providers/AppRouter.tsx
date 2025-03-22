0761196679f75cf466fed089ad757c4a8410c15b
Orobinsk <andy_prikol@mail.ru>
2025-03-22 17:07:23 +0300

    add layout

--- src/app/providers/AppRouter.tsx
+++ src/app/providers/AppRouter.tsx
@@ -1,11 +1,14 @@
 import { Main } from '@/pages/Main';
+import { routes } from '@/shared/const/routes';
+import { Layout } from '@/widget/layout';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path={routes.Main} element={<Layout />} />
+        <Route index element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
